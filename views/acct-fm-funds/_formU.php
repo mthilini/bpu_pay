74cc19f54c01022bce1105d1c0fd69efa1b8c031
Thilini Dilrukshi Mudalige <s.c.mudalige94@gmail.com>
2024-09-05 15:26:30 +0530

    Fund Management->Fund Types

--- views/acct-fm-funds/_formU.php
+++ views/acct-fm-funds/_formU.php
@@ -11,30 +11,48 @@ use yii\widgets\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="acct-fm-funds-form">
+<div class="row acct-fm-funds-form">
+    <div class="col-md-6 col-lg-5 col-xl-5">
+        <table width="100%" xmlns="http://www.w3.org/1999/html">
+            <tr>
+                <td valign="top">
+                    <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="panel-body">
 
-    <?php $form = ActiveForm::begin(); ?>
+                                <div class="user-view">
+                                    <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'fundName')->textInput(['maxlength' => true]) ?>
+                                    <?= $form->field($model, 'fundName')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'fundBankType')->textInput(['maxlength' => true]) ?>
+                                    <?= $form->field($model, 'fundBankType')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'fundBankCode')->dropDownList(
-            ArrayHelper::map(PayBank::find()->orderBy('bankName')->all(),'bankBank','bankName'),
-            ['prompt'=>'Select Bank Name']
-    ) ?>
+                                    <?= $form->field($model, 'fundBankCode')->dropDownList(
+                                        ArrayHelper::map(PayBank::find()->orderBy('bankName')->all(), 'bankBank', 'bankName'),
+                                        ['prompt' => 'Select Bank Name']
+                                    ) ?>
 
-    <?= $form->field($model, 'fundBankAcct')->textInput(['maxlength' => true]) ?>
+                                    <?= $form->field($model, 'fundBankAcct')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'fundLedg')->dropDownList(
-            ArrayHelper::map(AcctLedger::find()->orderBy('ledgDesc')->all(),'ledgCode','ledgDesc'),
-            ['prompt'=>'Select Ledger Code']
-    ) ?>
+                                    <?= $form->field($model, 'fundLedg')->dropDownList(
+                                        ArrayHelper::map(AcctLedger::find()->orderBy('ledgDesc')->all(), 'ledgCode', 'ledgDesc'),
+                                        ['prompt' => 'Select Ledger Code']
+                                    ) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+                                    <p>
+                                        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+                                        <?= Html::a('Close', ['/acct-fm-funds'], ['class' => 'btn btn-default pull-right']) ?>
+                                    </p>
+
+                                    <?php ActiveForm::end(); ?>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
--- views/acct-fm-funds/view.php
+++ views/acct-fm-funds/view.php
@@ -11,33 +11,48 @@ $this->params['breadcrumbs'][] = ['label' => 'Fund Management Types', 'url' => [
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="acct-fm-funds-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row acct-fm-funds-view">
+    <div class="col-md-6 col-lg-5 col-xl-5">
+        <table width="100%" xmlns="http://www.w3.org/1999/html">
+            <tr>
+                <td valign="top">
+                    <div class="box box-primary">
+                        <div class="box-body">
+                            <div class="panel-body">
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+                                <div class="user-view">
+                                    <?= DetailView::widget([
+                                        'model' => $model,
+                                        'attributes' => [
+                                            //'id',
+                                            'fundCode',
+                                            'fundName',
+                                            'fundBankType',
+                                            //'fundBankCode',
+                                            'fundBankCode0.bankName',
+                                            'fundBankAcct',
+                                            'fundLedg0.ledgDesc',
+                                        ],
+                                    ]) ?>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            //'id',
-            'fundCode',
-            'fundName',
-            'fundBankType',
-            //'fundBankCode',
-            'fundBankCode0.bankName',
-            'fundBankAcct',
-            'fundLedg0.ledgDesc',
-        ],
-    ]) ?>
-
+                                    <p>
+                                        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+                                        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+                                            'class' => 'btn btn-danger',
+                                            'data' => [
+                                                'confirm' => 'Are you sure you want to delete this item?',
+                                                'method' => 'post',
+                                            ],
+                                        ]) ?>
+                                        <?= Html::a('Close', ['/acct-fm-funds'], ['class' => 'btn btn-default pull-right']) ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
 </div>
