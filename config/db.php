e126763e5dfff0c67abe6345ece06ed58a679c79
mpriyadarshana <priyadiesh@gmail.com>
2024-09-12 14:18:05 +0530

    db pass

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bpuaccts',
     'username' => 'root',
-    'password' => 'teenwolf',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- config/db2.php
+++ config/db2.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=misbpudb',
     'username' => 'root',
-    'password' => 'teenwolf',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
