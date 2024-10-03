d3ee973c38de8c57991fcb1e4d47a1b337e6dda6
mpriyadarshana <priyadiesh@gmail.com>
2024-10-03 10:18:03 +0530

    sidebar sticky

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary position-fixed elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
