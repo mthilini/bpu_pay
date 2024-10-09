6fc42f2b1728c9f41a7f07b798a2d892cbd348c6
mpriyadarshana <priyadiesh@gmail.com>
2024-10-09 09:08:16 +0530

    login n employees model

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/site.css',
         'css/style.css',
+        'css/login.css',
     ];
     public $js = [
         'js/main.js'
